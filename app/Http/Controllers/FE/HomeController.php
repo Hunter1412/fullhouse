7d2658857d460d58133a314de20f7ca04f3e7d9d
huydeptroai <114966575+huydeptroai@users.noreply.github.com>
2023-05-04 15:32:26 +0700

    login

--- app/Http/Controllers/FE/HomeController.php
+++ app/Http/Controllers/FE/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\FE;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
 
 class HomeController extends Controller
 {
@@ -43,12 +44,44 @@ class HomeController extends Controller
         return view('fe.login');
     }
 
+    public function checkLogin(Request $request)
+    {
+        $email = $request->email;
+        $pwd = $request->password;
+
+        $user = Employee::where('email', $email)->first();
+        if ($user != null && $user->password == $pwd) {
+            if ($user->role == 1) {
+                return redirect('fe.home');
+            } else {
+                return redirect('admin');
+            }
+        } else {
+            return redirect('login');
+        }
+    }
+
     public function register()
     {
         return view('fe.register');
     }
+    
+    public function RegisterAcc(LoginRequest $request)
+    {
+        $item = $request->all();
+        $user = new User();
+        $user->name = $item->name;
+        $user->email = $item->email;
+        $user->phone = $item->phone;
+        $user->password = $item->password;
+        $user->save();
+        
+        return redirect('/fe');
+    }
+
     public function register_socialite()
     {
         return view('fe.register_socialite');
     }
+
 }
